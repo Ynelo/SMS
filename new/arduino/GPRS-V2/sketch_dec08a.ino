02a6e8848ad72b582003093015f8c42c71b4bdf1
i@ynelo.ru <Vova1290>
2023-12-08 21:38:59 +0300

    New Code

--- new/arduino/GPRS-V2/sketch_dec08a.ino
+++ new/arduino/GPRS-V2/sketch_dec08a.ino
@@ -14,7 +14,35 @@ char phone[16];
 // дата отправки сообщения
 char datetime[24];
 bool flag = false;
-String message;
+
+// длина сообщения
+#define MESSAGE_LENGTH 160
+#define MESSAGE_2ON  "2on"
+#define MESSAGE_0ON  "0on"
+
+#define MESSAGE_2OFF  "2off"
+#define MESSAGE_0OFF  "0off"
+
+#define MESSAGE_allon  "allon"
+#define MESSAGE_alloff  "alloff"
+#define MESSAGE_allreset  "allreset"
+
+#define MESSAGE_2reset  "2reset"
+#define MESSAGE_0reset  "0reset"
+
+#define MESSAGE_S1  "s1"
+#define MESSAGE_S2  "s2"
+#define MESSAGE_S3  "s3"
+#define MESSAGE_S4  "s4"
+#define MESSAGE_S5  "s5"
+
+#define MESSAGE_S5RESET  "s5reset"
+#define MESSAGE_S1RESET  "s1reset"
+#define MESSAGE_S2RESET  "s2reset"
+#define MESSAGE_S3RESET  "s3reset"
+#define MESSAGE_S4RESET  "s4reset"
+
+#define MESSAGE_ARDUINO  "arduino"
 
 SoftwareSerial mySerial(10, 11);
 //SoftwareSerial mySerial (0, 1);
@@ -61,10 +89,10 @@ void setup()
     // пока не появится ответ от GPRS-устройства
     delay(1000);
     Serial.print("GPRS Init error\r\n");
-    digitalWrite(12, HIGH);
   }
   // выводим сообщение об удачной инициализации GPRS Shield
   Serial.println("GPRS init success");
+
 }
 int vale;
 void loop()
@@ -72,116 +100,118 @@ void loop()
   vale = digitalRead(7);
   if(vale == 0 & flag == false){
    // gprs.sendSMS("+79097669661", "!!WARNING!! BLCAKOUT IN OFFICE!!!");
-    gprs.sendSMS(phone, "!!WARNING!! BLCAKOUT IN OFFICE!!!");
+  gprs.sendSMS(phone, "!!WARNING!! BLCAKOUT IN OFFICE!!!");
     flag = true;
     Serial.println("---------");
   } else if(vale == 1 & flag == true){
    // gprs.sendSMS("+79097669661", "The light turned on.");
-    gprs.sendSMS(phone, "The light turned on.");
+   gprs.sendSMS(phone, "The light turned on.");
     Serial.println("+++++++");
     flag = false;
    }
-/*  if (!gprs.init()) {
-   Serial.print("GPRS Init error\r\n");
-   digitalWrite(12, HIGH);
- } else {
-   digitalWrite(12, LOW);
-Serial.println("GPRS init success"); 
-}*/
+ /* while (!gprs.init()) {
+  //  gprs.sendSMS(phone, "I dead!");
+    Serial.print("GPRS Init error\r\n");
+   // gprs.powerOff();
+ //   delay(1500);
+   // gprs.powerOn();
+  }*/
   if (gprs.ifSMSNow()) {
     gprs.readSMS(massage, phone, datetime);
-    message = massage;
-    Serial.println(message);
-    if(message == "2Off" | message == "2off"){
-      digitalWrite(6, LOW);
-      gprs.sendSMS(phone, "2Off");
-    } else if(message == "2On" | message == "2on"){
-      digitalWrite(6, HIGH);
-      gprs.sendSMS(phone, "2On");
-    } else if(message == "0On" | message == "0on"){
-      digitalWrite(4, HIGH);
-      gprs.sendSMS(phone, "0On");
-    } else if(message == "0Off" | message == "0off"){
-      digitalWrite(4, LOW);
-      gprs.sendSMS(phone, "0Off");
-    } else if(message == "2Reset" | message == "2reset"){
-      digitalWrite(6, LOW);
-      delay(3000);
-      digitalWrite(6, HIGH);
-      gprs.sendSMS(phone, "2Reset");
-    } else if(message == "0Reset" | message == "0reset"){
+    Serial.print(massage);
+    Serial.print('|');
+    Serial.println(phone);
+    Serial.println("-----");
+    if (strcmp(massage, MESSAGE_0ON) == 0) {
+        digitalWrite(4, HIGH);
+        gprs.sendSMS(phone, MESSAGE_0ON);
+      } else if(strcmp(massage, MESSAGE_0OFF) == 0){
+        digitalWrite(4, LOW);
+        gprs.sendSMS(phone, MESSAGE_0OFF);
+      } else if(strcmp(massage, MESSAGE_2OFF) == 0){
+        digitalWrite(6, LOW);
+        gprs.sendSMS(phone, MESSAGE_2OFF);
+      } else if(strcmp(massage, MESSAGE_2ON) == 0){
+        digitalWrite(6, LOW);
+        gprs.sendSMS(phone, MESSAGE_2ON);
+      } else if(strcmp(massage, MESSAGE_0reset) == 0){
       digitalWrite(4, LOW);
       delay(3000);
       digitalWrite(4, HIGH);
-      gprs.sendSMS(phone, "0Reset");
-      } else if(message == "Allreset" | message == "allreset"){
+      gprs.sendSMS(phone, MESSAGE_0reset);
+      } else if(strcmp(massage, MESSAGE_2reset) == 0){
+      digitalWrite(6, LOW);
+      delay(3000);
+      digitalWrite(6, HIGH);
+      gprs.sendSMS(phone, MESSAGE_2reset);
+      } else if(strcmp(massage, MESSAGE_allreset) == 0){
       digitalWrite(4, LOW);
       digitalWrite(6, LOW);
       delay(3000);
       digitalWrite(4, HIGH);
       digitalWrite(6, LOW);
-      gprs.sendSMS(phone, "AllReset");
-      } else if(message == "Alloff" | message == "alloff"){
+      gprs.sendSMS(phone, MESSAGE_allreset);
+      } else if(strcmp(massage, MESSAGE_alloff) == 0){
       digitalWrite(4, LOW);
       digitalWrite(6, LOW);
-      gprs.sendSMS(phone, "Alloff");
-      } else if(message == "Allon" | message == "allon"){
+        gprs.sendSMS(phone, MESSAGE_alloff);
+      } else if(strcmp(massage, MESSAGE_allon) == 0){
       digitalWrite(4, HIGH);
       digitalWrite(6, HIGH);
-      gprs.sendSMS(phone, "Allon");
-    } else if(message == "arduino" | message == "Arduino"){
-      gprs.sendSMS(phone, "arduino Reset");
-      digitalWrite(5, HIGH);
-    } else if(message == "S1" | message == "s1"){
+        gprs.sendSMS(phone, MESSAGE_allon);
+      } else if(strcmp(massage, MESSAGE_S1) == 0){
       digitalWrite(12, HIGH);
       delay(3000);
       digitalWrite(12, LOW);
-      gprs.sendSMS(phone, "s1");
-    } else if(message == "S2" | message == "s2"){
+      gprs.sendSMS(phone, MESSAGE_S1);
+      } else if(strcmp(massage, MESSAGE_S2) == 0){
       digitalWrite(8, HIGH);
       delay(3000);
       digitalWrite(8, LOW);
-      gprs.sendSMS(phone, "s2");
-    } else if(message == "S3" | message == "s3"){
+      gprs.sendSMS(phone, MESSAGE_S2);
+      } else if(strcmp(massage, MESSAGE_S3) == 0){
       digitalWrite(9, HIGH);
       delay(3000);
       digitalWrite(9, LOW);
-      gprs.sendSMS(phone, "s3");
-    } else if(message == "S4" | message == "s4"){
+      gprs.sendSMS(phone, MESSAGE_S3);
+      } else if(strcmp(massage, MESSAGE_S4) == 0){
       digitalWrite(13, HIGH);
       delay(3000);
       digitalWrite(13, LOW);
-      gprs.sendSMS(phone, "s4");
-    } else if(message == "S5" | message == "s5"){
+      gprs.sendSMS(phone, MESSAGE_S4);
+      } else if(strcmp(massage, MESSAGE_S5) == 0){
       digitalWrite(19, HIGH);
       delay(3000);
       digitalWrite(19, LOW);
-      gprs.sendSMS(phone, "s5");
-    }  else if(message == "S1reset" | message == "s1reset"){
+      gprs.sendSMS(phone, MESSAGE_S5);
+      } else if(strcmp(massage, MESSAGE_S1RESET) == 0){
       digitalWrite(14, HIGH);
       delay(3000);
       digitalWrite(14, LOW);
-      gprs.sendSMS(phone, "s1reset");
-    }  else if(message == "S2reset" | message == "s2reset"){
+      gprs.sendSMS(phone, MESSAGE_S1RESET);
+      } else if(strcmp(massage, MESSAGE_S2RESET) == 0){
       digitalWrite(15, HIGH);
       delay(3000);
       digitalWrite(15, LOW);
-      gprs.sendSMS(phone, "s2reset");
-    }  else if(message == "S3reset" | message == "s3reset"){
+      gprs.sendSMS(phone, MESSAGE_S2RESET);
+      } else if(strcmp(massage, MESSAGE_S3RESET) == 0){
       digitalWrite(16, HIGH);
       delay(3000);
       digitalWrite(16, LOW);
-      gprs.sendSMS(phone, "s3reset");
-    }  else if(message == "S4reset" | message == "s4reset"){
+      gprs.sendSMS(phone, MESSAGE_S3RESET);
+      } else if(strcmp(massage, MESSAGE_S4RESET) == 0){
       digitalWrite(17, HIGH);
       delay(3000);
       digitalWrite(17, LOW);
-      gprs.sendSMS(phone, "s4reset");
-    }  else if(message == "S5reset" | message == "s5reset"){
+      gprs.sendSMS(phone, MESSAGE_S4RESET);
+      } else if(strcmp(massage, MESSAGE_S5RESET) == 0){
       digitalWrite(18, HIGH);
       delay(3000);
       digitalWrite(18, LOW);
-      gprs.sendSMS(phone, "s5reset");
+      gprs.sendSMS(phone, MESSAGE_S5RESET);
+      } else if(strcmp(massage, MESSAGE_ARDUINO) == 0){
+      gprs.sendSMS(phone, "Arduino Restart");
+      digitalWrite(5, HIGH);
+      }
     }
   }
-}
