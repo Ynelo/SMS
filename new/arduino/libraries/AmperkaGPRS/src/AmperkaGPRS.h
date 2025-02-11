e22d482a7c424e0283e1889ac324a9abe4a6f580
Ynelo <i@ynelo.ru>
2025-02-11 17:47:10 +0300

    Upd

--- new/arduino/libraries/AmperkaGPRS/src/AmperkaGPRS.h
+++
@@ -1,9 +0,0 @@
-#ifndef __AMPERKA_GPRS_H__
-#define __AMPERKA_GPRS_H__
-
-#include "GPRS.h"
-#include "GPRSClient.h"
-#include "GPRSServer.h"
-#include "./utility/uartDevice.h"
-
-#endif // __AMPERKA_GPRS_H__
